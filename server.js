7328f774931602752ca6143b1e9d79751b62552c
Nick Volpe <nvolpe@nvolpe-ltm1.internal.salesforce.com>
2017-12-31 01:10:05 -0500

    fix router?

--- server.js
+++ server.js
@@ -183,6 +183,9 @@ app.set('port', process.env.PORT || 3001);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  });
 }
 
 app.listen(app.get('port'));
