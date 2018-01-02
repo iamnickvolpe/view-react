74b988e7f5639bf2c6ea60c32fc04f642e57ad4b
Nick Volpe <nvolpe@nvolpe-ltm1.internal.salesforce.com>
2018-01-02 12:44:05 -0500

    events size. error handling. private events.

--- client/src/components/protected/widgets/Events.js
+++ client/src/components/protected/widgets/Events.js
@@ -6,14 +6,14 @@ class Events extends Component {
         return (
             <div className="events" style={{ backgroundColor: this.props.widget.settings.backgroundColor, color: this.props.widget.settings.color }} >
                 <div className="font-1 bold margin-1">{this.props.widget.settings.name}</div>
-                    {this.props.widget.data.items.map((event) => {
+                    {(this.props.widget.data && this.props.widget.data.items) ? this.props.widget.data.items.map((event) => {
                         return (
                             <div className="margin-2" key={event.id}>
-                                <div className="bold font-2 events__summary">{event.summary}</div>
+                                <div className="bold font-2 events__summary">{(event.summary.includes("private"))? 'Private event': event.summary}</div>
                                 <div className="normal font-3"><Moment format="dddd MMMM Do @ h:mma">{event.start.date || event.start.dateTime}</Moment> (<Moment fromNow>{event.start.date || event.start.dateTime}</Moment>)</div>
                             </div>
                         )
-                    })}
+                    }): null}
             </div>
         )
     }
